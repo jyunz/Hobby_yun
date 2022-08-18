8e9346ae88a8416b4733fa0bd6fe909e56927027
Roy <J.jyun0525@gmail.com>
2022-08-18 14:59:05 +0900

    탭레이아웃 + 뷰페이저 배치

--- app/src/main/java/com/roy/hobby_yun/MainActivity.kt
+++ app/src/main/java/com/roy/hobby_yun/MainActivity.kt
@@ -2,12 +2,17 @@ package com.roy.hobby_yun
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        setupEvents()
+        setupValues()
     }
 
     override fun setupEvents() {
@@ -15,6 +20,10 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupValues() {
+//        뷰페이저 작업 -> 탭레이아웃과 연결
+
+
+//        mainTabLayout.setupWithViewPager()
 
     }
 
